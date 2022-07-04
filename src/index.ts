103128443973816fe6853fe5cedc5d6fc9de7b12
Paolo Infante <info@paoloinfante.it>
2022-07-04 10:05:35 +0200

    added index file with re-exported types

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import MinesweeperBoard, {
+    DisplayedCell,
+    TestResult,
+} from "./minesweeper/board";
+import Matrix, { MatrixCell } from "./utils/matrix";
+
+export { MinesweeperBoard, DisplayedCell, TestResult, Matrix, MatrixCell };
